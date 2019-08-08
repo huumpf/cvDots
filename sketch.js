1f8d3bd077fe398ea62195b3489390d5ca3085e3
huumpf <nico@windwalk.de>
2019-08-08 14:59:32 +0200

    Working version

--- sketch.js
+++ sketch.js
@@ -1,22 +1,24 @@
+var DOT_COUNT = 5;
 var canvas;
-var looping = true;
+var size = 4;
 
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
+  noLoop();
+  noStroke();
+  background(255, 0);
 }
 
 function draw() {
-  background(220);
-}
 
-function keyTyped() {
-  if (key === " ") {
-    if (looping) {
-      noLoop();
-      looping = false;
-    } else {
-      loop();
-      looping = true;
-    }
+  fill(51, 153, 255);
+
+  for (let i = 0; i < DOT_COUNT; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+
+    rect(x, y, size, size);
+
+    fill(0, 40);
   }
 }
